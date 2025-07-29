bcdbea1bab26255e95a51a88c78ce3d93015f91a
sumit-kumarmahto <sumit-kumar.mahto@nagarro.com>
2025-07-29 22:29:02 +0530

    product

---
+++ src/main/java/com/example/product/ProductController.java
@@ -0,0 +1,21 @@
+package com.example.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return repository.findAll();
+    }
+}
